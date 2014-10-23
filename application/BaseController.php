79d221992aa235de3a308d100a32085582fcd9b0
Chau Ngo <ngokchau@gmail.com>
2014-10-23 01:19:16 -0700

    reindent code

--- application/BaseController.php
+++ application/BaseController.php
@@ -1,99 +1,99 @@
-<?php 
+<?php
 
 abstract class BaseController
 {
-	protected $template;
+    protected $template;
 
     /**
      * __construct
      * @param $template
      */
     function __construct($template)
-	{
-		$this->template = $template;
-	}
-
-	/**
-	 * index
-	 */
-	abstract function index();
-
-	/**
-	 * curl
-	 * @param string $url
-	 * @return json
-	 */
-	protected function curl($url)
     {
-		$ch = curl_init();
+        $this->template = $template;
+    }
+
+    /**
+     * index
+     */
+    abstract function index();
+
+    /**
+     * curl
+     * @param string $url
+     * @return json
+     */
+    protected function curl($url)
+    {
+        $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		$data = json_decode(curl_exec($ch));
-		curl_close($ch);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = json_decode(curl_exec($ch));
+        curl_close($ch);
 
-		return $data;
-	}
+        return $data;
+    }
 
-	/**
-	 * unsetCookies
-	 * @param array $cookies
-	 */
-	protected function unsetCookies(array $cookies)
-	{
-		foreach($cookies as $cookie)
-		{
-			setcookie($cookie, null, time() - 3600, "/");
-		}
-	}
-	
-	/**
-	 * redirect
-	 * @param string $location
-	 */
-	protected function redirect($location)
-	{
-		header("Location: $location");
-		exit();
-	}
-	
-	/**
-	 * redirectIfNullOrEmpty
-	 * @param mixed $var
-	 * @param string $location
-	 */
-	protected function redirectIfNullOrEmpty($var, $location)
-	{
-		if(is_null($var) || empty($var))
-		{
-			$this->redirect($location);
-		}
-	}
-
-	/**
-	 * TODO: update Log object to use static method
-	 * @param string $method
-	 * @return json
-	 */
-	protected function log($method)
+    /**
+     * unsetCookies
+     * @param array $cookies
+     */
+    protected function unsetCookies(array $cookies)
     {
-		$remote_addr = filter_input(INPUT_SERVER, "REMOTE_ADDR");
-		$user_agent = filter_input(INPUT_SERVER, "HTTP_USER_AGENT");
-		$data = $this->curl('http://ipinfo.io/' . $remote_addr);
+        foreach($cookies as $cookie)
+        {
+            setcookie($cookie, null, time() - 3600, "/");
+        }
+    }
 
-		if($remote_addr != "::1" && 0 !== strpos($remote_addr, "192.168.")) {
-			$log = new log($this->template);
-			$log->created_on = date("Y-m-d h:i:s");
-			$log->viewed = $method;
-			$log->remote_addr = $remote_addr;
-			$log->user_agent = $user_agent;
-			$log->loc = $data->{"loc"};
-			$log->country = $data->{"country"};
-			$log->state = $data->{"region"};
-			$log->city = $data->{"city"};
-			$log->save();
-		}
+    /**
+     * redirect
+     * @param string $location
+     */
+    protected function redirect($location)
+    {
+        header("Location: $location");
+        exit();
+    }
 
-		return $data;
-	}
+    /**
+     * redirectIfNullOrEmpty
+     * @param mixed $var
+     * @param string $location
+     */
+    protected function redirectIfNullOrEmpty($var, $location)
+    {
+        if(is_null($var) || empty($var))
+        {
+            $this->redirect($location);
+        }
+    }
+
+    /**
+     * TODO: update Log object to use static method
+     * @param string $method
+     * @return json
+     */
+    protected function log($method)
+    {
+        $remote_addr = filter_input(INPUT_SERVER, "REMOTE_ADDR");
+        $user_agent = filter_input(INPUT_SERVER, "HTTP_USER_AGENT");
+        $data = $this->curl('http://ipinfo.io/' . $remote_addr);
+
+        if($remote_addr != "::1" && 0 !== strpos($remote_addr, "192.168.")) {
+            $log = new log($this->template);
+            $log->created_on = date("Y-m-d h:i:s");
+            $log->viewed = $method;
+            $log->remote_addr = $remote_addr;
+            $log->user_agent = $user_agent;
+            $log->loc = $data->{"loc"};
+            $log->country = $data->{"country"};
+            $log->state = $data->{"region"};
+            $log->city = $data->{"city"};
+            $log->save();
+        }
+
+        return $data;
+    }
 }
--- application/BaseDataAccess.php
+++ application/BaseDataAccess.php
@@ -2,246 +2,245 @@
 
 class BaseDataAccess
 {
-	protected $table;
-	protected $primary_key;
+    protected $table;
+    protected $primary_key;
 
-	/**
-	 * find
-	 * @return array of objects
-	 */
-	public static function find()
-	{
-		$object = new static;
+    /**
+     * find
+     * @return array of objects
+     */
+    public static function find()
+    {
+        $object = new static;
 
-		$sql = 'select '.$object->getPropertiesString().' '.
-			   'from '.$object->table.';';
+        $sql = 'select '.$object->getPropertiesString().' '.
+            'from '.$object->table.';';
 
-		return $object->fetch($sql, array());
-	}
+        return $object->fetch($sql, array());
+    }
 
-	/**
-	 * findById
-	 * @param mixed $id
-	 * @return object
-	 */
-	public static function findById($id) {
-		$object = new static;
-		
-		$sql = 'select ' . $object->getPropertiesString() . ' ' .
-			   'from ' . $object->table . ' ' .
-			   'where ' . $object->primary_key . ' = ?;';
-		
-		return $object->fetchSingle($sql, array($id));
-	}
+    /**
+     * findById
+     * @param mixed $id
+     * @return object
+     */
+    public static function findById($id) {
+        $object = new static;
 
-	/**
-	 * findByProperties
-	 * @param array $properties
-	 * @return array of objects
-	 */
-	public static function findByProperties(array $properties) {
-		$object = new static;
-		$filters = array();
-		$values = array();
-		
-		$sql = 'select ' . $object->getPropertiesString() . ' ' .
-			   'from ' . $object->table . ' ' .
-			   'where ';
-		
-		foreach($properties as $key => $value) {
-			$filters[] = $key . ' = ?';
-			$values[] = $value;
-		}
-		
-		$sql .= join(' and ', $filters) . ';';
-		
-		return $object->fetch($sql, $values);
-	}
+        $sql = 'select ' . $object->getPropertiesString() . ' ' .
+            'from ' . $object->table . ' ' .
+            'where ' . $object->primary_key . ' = ?;';
 
-	/**
-	 * insert
-	 * @return bool
-	 */
-	public function insert()
-	{
-		$properties = $this->getProperties();
-		$tokens = array();
-		$values = array();
-		
-		foreach($properties as $property) {
-			$name = $property->name;
-			
-			if($name != 'table' && $name != 'primary_key' && $name != 'id') {
-				$tokens[] = $name;
-				$values[] = '?';
-			}
-		}
-		
-		$sql = 'insert into ' . $this->table . '(' . join(', ', $tokens) . ') '.
-			   'values(' . join(', ', $values) . ');';
-		
-		$db = db::getInstance();
-		$stmt = $db->prepare($sql);
-		$stmt->execute($this->bindProperties());
-		$this->id = $db->lastInsertId();
-		
-		$error = $stmt->errorInfo();
-		return ($error[0] === '00000');
-	}
+        return $object->fetchSingle($sql, array($id));
+    }
 
-	/**
-	 * update
-	 * @return bool
-	 */
-	public function update() {
-		$properties = $this->getProperties();
-		$tokens = array();
-		
-		foreach($properties as $property) {
-			$name = $property->name;
-			
-			if($name != 'table' && $name != 'primary_key' && $name != 'id') {
-				$tokens[] = $name.' = ?';
-			}
-		}
-		
-		$sql = 'update ' . $this->table . ' ' .
-			   'set ' . join(', ', $tokens) . ' ' .
-			   'where ' . $this->primary_key . ' = ?;';
-		
-		$db = db::getInstance();
-		$stmt = $db->prepare($sql);
-		$stmt->execute($this->bindProperties(true));
-		
-		$error = $stmt->errorInfo();
-		return ($error[0] === '00000');
-	}
+    /**
+     * findByProperties
+     * @param array $properties
+     * @return array of objects
+     */
+    public static function findByProperties(array $properties) {
+        $object = new static;
+        $filters = array();
+        $values = array();
 
-	/**
-	 * delete
-	 * @return bool
-	 */
-	public function delete() {
-		$sql = 'delete from ' . $this->table . ' ' .
-			   'where ' . $this->primary_key . ' = ?;';
+        $sql = 'select ' . $object->getPropertiesString() . ' ' .
+            'from ' . $object->table . ' ' .
+            'where ';
 
-		$db = db::getInstance();
-		$stmt = $db->prepare($sql);
-		$properties_array = $this->bindProperties(true);
-		$primary_key = array_pop($properties_array);
-		$stmt->execute(array($primary_key));
+        foreach($properties as $key => $value) {
+            $filters[] = $key . ' = ?';
+            $values[] = $value;
+        }
+
+        $sql .= join(' and ', $filters) . ';';
+
+        return $object->fetch($sql, $values);
+    }
+
+    /**
+     * insert
+     * @return bool
+     */
+    public function insert()
+    {
+        $properties = $this->getProperties();
+        $tokens = array();
+        $values = array();
+
+        foreach($properties as $property) {
+            $name = $property->name;
+
+            if($name != 'table' && $name != 'primary_key' && $name != 'id') {
+                $tokens[] = $name;
+                $values[] = '?';
+            }
+        }
+
+        $sql = 'insert into ' . $this->table . '(' . join(', ', $tokens) . ') '.
+            'values(' . join(', ', $values) . ');';
+
+        $db = db::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->execute($this->bindProperties());
+        $this->id = $db->lastInsertId();
 
         $error = $stmt->errorInfo();
-		return ($error[0] === '00000');
-	}
+        return ($error[0] === '00000');
+    }
 
-	/**
-	 * bindProperties
-	 * @param bool $with_id
-	 * @return array
-	 */
-	protected function bindProperties($with_id = false)
-	{
-		$properties = $this->getProperties();
-		$primary = null;
-		$pairs = array();
-		
-		foreach($properties as $property) {
-			$name = $property->name;
-			
-			if($name != 'table' && $name != 'primary_key') {
-				if($name === $this->primary_key) {
-					$primary = $property->getValue($this);
-				}
-				else {
-					$pairs[] = $property->getValue($this);
-				}
-			}
-		}
-		
-		if($with_id) {
-			$pairs[] = $primary;
-		}
-		
-		return $pairs;
-	}
+    /**
+     * update
+     * @return bool
+     */
+    public function update() {
+        $properties = $this->getProperties();
+        $tokens = array();
 
-	/**
-	 * getPropertiesString
-	 * @return string
-	 */
-	protected function getPropertiesString()
-	{
-		$properties = $this->getProperties();
-		$columns = array();
+        foreach($properties as $property) {
+            $name = $property->name;
 
-		foreach($properties as $property) {
-			$name = $property->name;
+            if($name != 'table' && $name != 'primary_key' && $name != 'id') {
+                $tokens[] = $name.' = ?';
+            }
+        }
 
-			if($name != 'table' && $name != 'primary_key') {
-				$columns[] = $name;
-			}
-		}
+        $sql = 'update ' . $this->table . ' ' .
+            'set ' . join(', ', $tokens) . ' ' .
+            'where ' . $this->primary_key . ' = ?;';
 
-		return join(', ', $columns);
-	}
-	
-	protected function getProperties() {
-		$object = new static;
-		$reflector = new ReflectionClass($object);
-		return $reflector->getProperties();
-	}
+        $db = db::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->execute($this->bindProperties(true));
 
-	/**
-	 * fetch
-	 * @param string $sql
-	 * @param array $params
-	 * @return array of objects
-	 */
-	protected function fetch($sql, array $params)
-	{
-		$db = db::getInstance();	
-		$stmt = $db->prepare($sql);
-		$stmt->execute($params);
-		$objects = array();
+        $error = $stmt->errorInfo();
+        return ($error[0] === '00000');
+    }
 
-		while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			$objects[] = self::mapProperties($row);
-		}
+    /**
+     * delete
+     * @return bool
+     */
+    public function delete() {
+        $sql = 'delete from ' . $this->table . ' ' .
+            'where ' . $this->primary_key . ' = ?;';
 
-		return $objects;
-	}
+        $db = db::getInstance();
+        $stmt = $db->prepare($sql);
+        $properties_array = $this->bindProperties(true);
+        $primary_key = array_pop($properties_array);
+        $stmt->execute(array($primary_key));
 
-	/**
-	 * fetchSingle
-	 * @param string $sql
-	 * @param array $params
-	 * @return object
-	 */
-	protected function fetchSingle($sql, array $params) {
-		$db = db::getInstance();
-		$stmt = $db->prepare($sql);
-		$stmt->execute($params);
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
-		return ($result)
-			? self::mapProperties($result)
-			: null;
-	}
+        $error = $stmt->errorInfo();
+        return ($error[0] === '00000');
+    }
 
-	/**
-	 * mapProperties
-	 * @param array $properties
-	 * @return object
-	 */
-	protected function mapProperties(array $properties)
-	{
-		$object = new static;
+    /**
+     * bindProperties
+     * @param bool $with_id
+     * @return array
+     */
+    protected function bindProperties($with_id = false)
+    {
+        $properties = $this->getProperties();
+        $primary = null;
+        $pairs = array();
 
-		foreach($properties as $key => $value) {
-			$object->$key = $value;
-		}
+        foreach($properties as $property) {
+            $name = $property->name;
 
-		return $object;
-	}
+            if($name != 'table' && $name != 'primary_key') {
+                if($name === $this->primary_key) {
+                    $primary = $property->getValue($this);
+                }
+                else {
+                    $pairs[] = $property->getValue($this);
+                }
+            }
+        }
 
+        if($with_id) {
+            $pairs[] = $primary;
+        }
+
+        return $pairs;
+    }
+
+    /**
+     * getPropertiesString
+     * @return string
+     */
+    protected function getPropertiesString()
+    {
+        $properties = $this->getProperties();
+        $columns = array();
+
+        foreach($properties as $property) {
+            $name = $property->name;
+
+            if($name != 'table' && $name != 'primary_key') {
+                $columns[] = $name;
+            }
+        }
+
+        return join(', ', $columns);
+    }
+
+    protected function getProperties() {
+        $object = new static;
+        $reflector = new ReflectionClass($object);
+        return $reflector->getProperties();
+    }
+
+    /**
+     * fetch
+     * @param string $sql
+     * @param array $params
+     * @return array of objects
+     */
+    protected function fetch($sql, array $params)
+    {
+        $db = db::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->execute($params);
+        $objects = array();
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $objects[] = self::mapProperties($row);
+        }
+
+        return $objects;
+    }
+
+    /**
+     * fetchSingle
+     * @param string $sql
+     * @param array $params
+     * @return object
+     */
+    protected function fetchSingle($sql, array $params) {
+        $db = db::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->execute($params);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return ($result)
+            ? self::mapProperties($result)
+            : null;
+    }
+
+    /**
+     * mapProperties
+     * @param array $properties
+     * @return object
+     */
+    protected function mapProperties(array $properties)
+    {
+        $object = new static;
+
+        foreach($properties as $key => $value) {
+            $object->$key = $value;
+        }
+
+        return $object;
+    }
 }
