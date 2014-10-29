6e3303615f120ce5e43289792ea2dfe375637dbf
Chau Ngo <ngokchau@gmail.com>
2014-10-28 21:25:39 -0700

    minor syntax format sql file

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -8,42 +8,42 @@ CREATE TABLE tblAccount
   FirstName varchar(50) NOT NULL,
   LastName varchar(50) NOT NULL,
   EmailAddress varchar(254) NOT NULL,
-  PassHashed bit(512) NOT NULL, --USE A SHA2 FUNCTION ON THE "USERPASSWORD + Salt" that returns 512 bit
-  Salt bit(512) NOT NULL, --RANDOMLY GENERATED
+  PassHashed varchar(512) NOT NULL,
+  Salt varchar(512) NOT NULL,
   CreatedOn DATETIME NOT NULL,
   PRIMARY KEY (AccountID)
 );
 
 CREATE TABLE tblEventType
 (
-  EventTypeID int not null AUTO_INCREMENT,
-  EventTypeName varchar(50) not null,
-  EventTypeDesc varchar(250) not null,
-  PRIMARY KEY (tblEventTypeID)
+  EventTypeID int NOT NULL AUTO_INCREMENT,
+  EventTypeName varchar(50) NOT NULL,
+  EventTypeDesc varchar(250) NOT NULL,
+  PRIMARY KEY (EventTypeID)
 );
 
 CREATE TABLE tblEvent 
 (
-  EventID int not null AUTO_INCREMENT,
-  UserID int not null foreign key references tblAccount (AccountID),
+  EventID int NOT NULL AUTO_INCREMENT,
+  UserID int NOT NULL FOREIGN KEY REFERENCES tblAccount (AccountID),
   EventStart DATETIME NOT NULL,
   EventEnd DATETIME NOT NULL,
   LocationString varchar(250),
-  EventTypeID int not null references tblEventType (EventTypeID),
+  EventTypeID int NOT NULL REFERENCES tblEventType (EventTypeID),
   PRIMARY KEY (EventID)
 );
 
 CREATE TABLE tblGroup
 (
-  GroupID int not null AUTO_INCREMENT,
-  GroupName varchar(50) not null,
+  GroupID int NOT NULL AUTO_INCREMENT,
+  GroupName varchar(50) NOT NULL,
   PRIMARY KEY (GroupID)
 );
 
 CREATE TABLE accountGroup
 (
-  AccountGroupID int not null AUTO_INCREMENT,
-  AccountID int not null references tblAccount (AccountID),
-  GroupID int not null references tblGroup (GroupID),
+  AccountGroupID int NOT NULL AUTO_INCREMENT,
+  accountid int NOT NULL REFERENCES tblAccount (AccountID),
+  GroupID int NOT NULL REFERENCES tblGroup (GroupID),
   PRIMARY KEY (AccountGroupID)
 );
