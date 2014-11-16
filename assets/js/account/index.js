528ff092ab7abea66d07faf86790b172ce1a1cc0
Chau Ngo <ngokchau@gmail.com>
2014-11-15 16:43:14 -0800

    fix angularjs bug

--- assets/js/account/index.js
+++ assets/js/account/index.js
@@ -1,3 +1,10 @@
-/**
- * Created by ngokchau on 11/15/14.
- */
+(function() {
+    var currentPathParts = window.location.pathname.split('/');
+    var projectName = currentPathParts[1];
+    var C = angular.module(projectName, []);
+
+    C.controller('BodyCtrl', [function() {
+
+    }]);
+})();
+
--- views/partials/header.php
+++ views/partials/header.php
@@ -5,7 +5,7 @@
 
     <link rel="stylesheet" href="/<?php echo __PROJECT_NAME; ?>/assets/css/app.css">
 
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.2/angular.min.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body ng-controller="BodyCtrl as Body">
