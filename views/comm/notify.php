0ad3b5a49f785a6bcdab115abb1ca676c031f2a9
Bryant Taylor <btaylor5@uw.edu>
2014-11-17 01:19:08 -0800

    wizard of ozing the feature email group about meeting

--- views/comm/notify.php
+++ views/comm/notify.php
@@ -3,11 +3,11 @@
 <form>
     <label>Invite to meeting:</label><textarea> Bryant, Hamzah, Chau, Keegan </textarea>
 
-    <label>Meeting Location:</label><textarea> BLD 070 </textare>
+    <label>Meeting Location:</label><textarea> BLD 070 </textarea>
 
     <label>Custom Message:</label><textarea> </textarea>
 
-        <button type=button" onclick="window.close(); alert("email sent")">Send</button>
+    <button type=button" onclick="window.close(); alert("email sent")">Send</button>
 </form>
 
 
