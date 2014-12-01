3808f5bbf51c3ac0d95d46159b619bb1b0abb480
Keegan Farley <keegomyneego@live.com>
2014-11-30 19:01:55 -0800

    another round of functionality updates

--- views/app/weekview.php
+++ views/app/weekview.php
@@ -1,7 +1,22 @@
 <?php include 'views/partials/header.php'; ?>
 
 <div class="header small">
-    <img src="/<?php echo __PROJECT_NAME; ?>/assets/images/cockamamie_logo_small.png" alt="Cockamamie">
+  <img src="/<?php echo __PROJECT_NAME; ?>/assets/images/cockamamie_logo_small.png" alt="Cockamamie">
+</div>
+
+<div class="navmenu">
+  <ul>
+    <li>
+      <a class="clickable" id="help">Help</a>
+    </li>
+    <li>
+      <a class="clickable" href="/cockamamei">Switch User</a>
+    </li>
+  </ul>
+</div>
+
+<div id="helpmenu">
+  <img src="/<?php echo __PROJECT_NAME; ?>/assets/images/diagram-mouse.png" alt="Left click marks a timeslot as free and right click marks it as busy.">
 </div>
 
 <div id="events">
@@ -13,31 +28,36 @@
   </ul>
 </div>
 
-<div>
-  
-</div>
 
 <div id="weekview-container">
-  <div class="clickable">
+  <div class="change-week noselect" id="prevWeek">
+    <span>&lt;</span>
+  </div>
+
+  <div class="day clickable noselect">
     <h1>Sunday</h1>
   </div>
-  <div class="clickable">
+  <div class="day clickable noselect">
     <h1>Monday</h1>
   </div>
-  <div class="clickable">
+  <div class="day clickable noselect">
     <h1>Tuesday</h1>
   </div>
-  <div class="clickable">
+  <div class="day clickable noselect">
     <h1>Wednesday</h1>
   </div>
-  <div class="clickable">
+  <div class="day clickable noselect">
     <h1>Thursday</h1>
   </div>
-  <div class="clickable">
+  <div class="day clickable noselect">
     <h1>Friday</h1>
   </div>
-  <div class="clickable">
+  <div class="day clickable noselect">
     <h1>Saturday</h1>
+  </div>
+
+  <div class="change-week noselect" id="nextWeek">
+    <span>&gt;</span>
   </div>
 </div>
 
